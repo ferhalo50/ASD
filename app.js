2e8848a2e9a0769ab3af69e0bc8888d35513c35d
ferhalo50 <48362025+ferhalo50@users.noreply.github.com>
2026-05-21 22:05:08 -0700

    First Update May/21/2026
    Pagina web de ASD

--- app.js
+++ app.js
@@ -16,63 +16,6 @@ boton.addEventListener("click", function(){
 
 });
 
-
-// CALCULADORA
-
-const calcularBtn = document.getElementById("calcularBtn");
-
-const calculadora = document.getElementById("calculadora");
-
-calcularBtn.addEventListener("click", function(){
-
-    if(calculadora.style.display === "block"){
-
-        calculadora.style.display = "none";
-
-    }else{
-
-        calculadora.style.display = "block";
-
-    }
-
-});
-
-
-const botonCalcular = document.getElementById("calcularAhorro");
-
-const resultado = document.getElementById("resultado");
-
-botonCalcular.addEventListener("click", function(){
-
-    const recibo = document.getElementById("recibo").value;
-
-    const ahorro = recibo * 12 * 0.70;
-
-    resultado.innerHTML =
-    "Podrías ahorrar hasta un 70% en tu recibo de luz, " +
-    "equivalente a aproximadamente $" +
-    ahorro.toFixed(2) +
-    " pesos al año ☀️";
-
-});
-
-function abrirImagen(src){
-
-    const visor = document.getElementById("visor");
-
-    const imagen = document.getElementById("imagenExpandida");
-
-    visor.style.display = "flex";
-
-    imagen.src = src;
-}
-
-
-function cerrarImagen(){
-
-    document.getElementById("visor").style.display = "none";
-}
-
 // MENU RESPONSIVE
 
 const menuToggle = document.getElementById("menuToggle");
